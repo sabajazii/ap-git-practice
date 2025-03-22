4ef31d08824d323ef4786e4212bfd1775cd8c357
saba <sababehzad85@gmail.com>
2025-03-22 21:46:56 +0330

    bighello

---
+++ BigHello.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void red(){printf("31m");}
+void Purple(){printf("35m");}
+void Blue (){printf("34m");}
+int main () {
+red();
+printf("Hello ");
+Purple();
+printf("world");
+Blue();
+printf("!!!
+");
+return 0;
+}
